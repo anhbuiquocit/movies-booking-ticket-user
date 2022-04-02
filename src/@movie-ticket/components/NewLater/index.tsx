5a10f179d10bd8f12e55a235a10c46872ff7eb00
anhbuiquocit <56730423+anhbuiquocit@users.noreply.github.com>
2022-04-02 21:18:16 +0700

    feat: new component newLater

--- src/@movie-ticket/components/NewLater/index.tsx
+++ src/@movie-ticket/components/NewLater/index.tsx
@@ -1,31 +1,42 @@
-import { Formik, FormikHelpers } from "formik"
-import { Input } from 'antd';
-interface Props{
-    onSubmit?: any,
+import { Input, Button } from "antd";
+import { useForm, Controller } from "react-hook-form";
+interface Props {
+  onSubmit?: any;
 }
+type FormData = {
+  email: string;
+};
 const NewLater = (props: Props) => {
-
-    return (
-        <div className="newlater-section padding-bottom">
-        <div className="grid">
-            <div className="newlater-container bg_img">
-                <div className="newslater-Fwrapper">
-                    <h5>SUBCRIBE TO BOLETO</h5> 
-                    <h3>TO GET EXCLUSIVE BENIFITS</h3>
-                    <Formik
-                        initialValues={{
-                            email: '',
-                        }}
-                        onSubmit={props.onSubmit}
-                        render={formikProps => {
-                            const {setFie}
-                        }}
-                    />
-                </div>
-            </div>
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({});
+  const onSubmit = (data: any) => {
+    console.log("dataSubmit: ", data);
+  };
+  return (
+    <div className="newlater-section padding-bottom">
+      <div className="grid">
+        <div className="newlater-container bg_img">
+          <div className="newslater-Fwrapper">
+            <h5>SUBCRIBE TO BOLETO</h5>
+            <h3>TO GET EXCLUSIVE BENIFITS</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Input
+                onChange={(val) => {
+                  console.log("value: ", val.target.value);
+                  setValue("email", val.target.value);
+                }}
+              />
+              {/* <Button type="submit" /> */}
+              <input type="submit" />
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-
-    )
-}
-export default NewLater
+  );
+};
+export default NewLater;
